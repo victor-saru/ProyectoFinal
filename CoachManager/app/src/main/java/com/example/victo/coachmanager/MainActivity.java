18c09c653ce8c6bb3cf846013698433ae936ef4a
victor-saru <victor-saru@hotmail.com>
2018-05-09 17:10:46 +0200

    OnClick Login

--- CoachManager/app/src/main/java/com/example/victo/coachmanager/MainActivity.java
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/MainActivity.java
@@ -6,10 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnRegistrase;
+    private Button btnLogin;
+    private Connection conexion;
+    private String URL = "jdbc:mysql://localhost:3306/sakila";
+    private String IP = "127.0.0.1";
+    private int PUERTO = 3306;
+    private String BD = "coachmanager";
+    private String USER = "root";
+    private String PASSWORD = "";
+    private Statement statement;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +32,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnRegistrase = (Button) (findViewById(R.id.btnRegistrase));
-        Log.e("kk", "1");
+        conexion = null;
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+
+        conexionBD();
+
+
 
         btnRegistrase.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +48,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeIntent();
+            }
+        });
+
+    }
+
+    private void conexionBD() {
+
+        try {
+            Class.forName("mysql-connector-java-5.1.28-bin.jar").newInstance ();
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connexió a la BD realitzada!");
+        }catch(SQLException e){
+            System.err.println("Error de connexió amb la BD: " + e.getMessage());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -43,6 +89,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(id == btnRegistrase.getId()){
             Log.e("kk", "3");
             Intent i = new Intent(this, RegistroActivity.class);
+            startActivity(i);
+        }
+
+        if(id == btnLogin.getId()){
+            Intent i = new Intent(this, MenuActivity.class);
             startActivity(i);
         }
 
