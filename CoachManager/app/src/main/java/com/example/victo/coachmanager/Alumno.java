7ca487186fc9e4333264490d6e89921d530d2d29
Óscar Gómez Fernández <oscaramelo_22@hotmail.com>
2018-05-14 18:02:05 +0200

    Maquetación alumnos 14/05
    El ListView de los alumnos está hecho, la información de estos está recién empezada.

---
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/Alumno.java
@@ -0,0 +1,126 @@
+package com.example.victo.coachmanager;
+
+/**
+ * Created by Oscar on 14/05/2018.
+ */
+
+public class Alumno {
+
+    String nombre, primer_apellido, segundo_apellido, dni, fecha_nacimiento, genero, mano_dom, pie_dom, observaciones;
+    int movil, peso, altura;
+
+    /*public Alumno() {
+        super();
+    }*/
+
+    public Alumno(String nombre, String primer_apellido, String segundo_apellido, String dni, String fecha_nacimiento, String genero, String mano_dom, String pie_dom, String observaciones, int movil, int peso, int altura) {
+        this.nombre = nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+        this.dni = dni;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.genero = genero;
+        this.mano_dom = mano_dom;
+        this.pie_dom = pie_dom;
+        this.observaciones = observaciones;
+        this.movil = movil;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimer_apellido() {
+        return primer_apellido;
+    }
+
+    public void setPrimer_apellido(String primer_apellido) {
+        this.primer_apellido = primer_apellido;
+    }
+
+    public String getSegundo_apellido() {
+        return segundo_apellido;
+    }
+
+    public void setSegundo_apellido(String segundo_apellido) {
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getMano_dom() {
+        return mano_dom;
+    }
+
+    public void setMano_dom(String mano_dom) {
+        this.mano_dom = mano_dom;
+    }
+
+    public String getPie_dom() {
+        return pie_dom;
+    }
+
+    public void setPie_dom(String pie_dom) {
+        this.pie_dom = pie_dom;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public int getMovil() {
+        return movil;
+    }
+
+    public void setMovil(int movil) {
+        this.movil = movil;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+}
--- CoachManager/app/src/main/java/com/example/victo/coachmanager/Alumnos.java
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/Alumnos.java
@@ -1,27 +1,52 @@
 package com.example.victo.coachmanager;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Alumnos extends AppCompatActivity {
+
+    ArrayList<Alumno> al_alumnos;
+    ListView lista_alumnos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumnos);
 
-        ArrayList<String> listItems=new ArrayList<String>();
-        ArrayAdapter<String> adapter;
+        int id;
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
-        adapter.add("Hola");
+        al_alumnos = new ArrayList<Alumno>();
+        lista_alumnos = (ListView) findViewById(R.id.lv_lista_alumnos);
+        AdapterAlumno adapter = new AdapterAlumno(this, al_alumnos);
 
+        Alumno a1 = new Alumno("Óscar", "Gómez", "Fernández", "49222336J", "22/02/1997", "Indefinido", "Indefinida", "Indefinido", "Es un poco tonto, la verdad.", 667348629, 100000, 100000);
 
+        al_alumnos.add(a1);
+
+        lista_alumnos.setAdapter(adapter);
+
+        lista_alumnos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Alumno a0;
+                a0 = (Alumno) lista_alumnos.getItemAtPosition(position);
+
+                Intent intent = new Intent(Alumnos.this,VerAlumno.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
