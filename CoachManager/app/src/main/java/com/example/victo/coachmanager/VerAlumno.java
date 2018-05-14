99d6c34c5dee5ae44ac560775f4cc58f8d3261fa
Óscar Gómez Fernández <oscaramelo_22@hotmail.com>
2018-05-14 20:01:52 +0200

    Ver alumno maquetado

--- CoachManager/app/src/main/java/com/example/victo/coachmanager/VerAlumno.java
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/VerAlumno.java
@@ -1,7 +1,10 @@
 package com.example.victo.coachmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerAlumno extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class VerAlumno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_alumno);
+
+        Button btnVolver = (Button) findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VerAlumno.this, Alumnos.class));
+            }
+        });
     }
 }
