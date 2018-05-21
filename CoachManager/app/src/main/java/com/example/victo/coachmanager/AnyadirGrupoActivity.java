f0718d940e3a6dbceb7c131f6b44c913b4e1e8ed
victor-saru <victor-saru@hotmail.com>
2018-05-21 12:46:53 +0200

    Añadir entrenador a grupos, ejercicios, alumnos y empezar añadir grupo

--- CoachManager/app/src/main/java/com/example/victo/coachmanager/AnyadirGrupoActivity.java
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/AnyadirGrupoActivity.java
@@ -3,22 +3,37 @@ package com.example.victo.coachmanager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.victo.coachmanager.Entidades.Alumno;
+import com.example.victo.coachmanager.Entidades.VolleySingleton;
 
-public class AnyadirGrupoActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class AnyadirGrupoActivity extends AppCompatActivity implements View.OnClickListener , Response.Listener<JSONObject>, Response.ErrorListener{
 
     EditText edNombreGrupo;
     EditText edCategoriaGrupo;
     Button btnAñadirGrupo;
     ListView lv_alumnos_grupo;
+    ImageView btnVolver;
+    ArrayList<Alumno> al_alumnos;
 
     String resultado;
     RequestQueue request;
@@ -33,29 +48,119 @@ public class AnyadirGrupoActivity extends AppCompatActivity {
         edCategoriaGrupo = (EditText) findViewById(R.id.edCategoriaGrupo);
         btnAñadirGrupo = (Button) findViewById(R.id.btnAñadirGrupo);
         lv_alumnos_grupo = (ListView)findViewById(R.id.lv_lista_alumnos);
+        btnVolver = (ImageView) findViewById(R.id.btnVolver);
+
+        btnAñadirGrupo.setOnClickListener(this);
+        btnVolver.setOnClickListener(this);
 
         request = Volley.newRequestQueue(getApplicationContext());
 
-        btnAñadirGrupo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AnyadirGrupoActivity.this, GruposActivity.class);
-                startActivityForResult(intent,1);
-            }
-        });
 
-        ImageView btnVolver = (ImageView) findViewById(R.id.btnVolver);
 
-        btnVolver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AnyadirGrupoActivity.this, GruposActivity.class);
-                startActivityForResult(intent,1);
-            }
-        });
+
     }
+
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         finish();
     }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnAñadirAlumno:{
+                comprobarValores();
+                Intent intent = new Intent(AnyadirGrupoActivity.this, GruposActivity.class);
+                startActivityForResult(intent,1);
+                break;
+            }
+
+            case R.id.btnVolver:{
+                Intent intent = new Intent(AnyadirGrupoActivity.this, GruposActivity.class);
+                startActivityForResult(intent,1);
+                break;
+            }
+        }
+    }
+
+    private void comprobarValores() {
+
+        if(edNombreGrupo.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Tienes que rellenar todos los campos obligatorios", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(comprovarIntegerYString(edNombreGrupo.getText().toString())){
+            Toast.makeText(getApplicationContext(), "Tienes que introducir un nombre correcto", Toast.LENGTH_SHORT).show();
+        }
+        
+        else{
+            cargarWebService();
+        }
+        
+    }
+
+    private void cargarWebService() {
+        String url="http://192.168.1.45/CoachManagerPHP/CoachManager_Alumnos.php";
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        VolleySingleton.getIntanciaVolley(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private boolean comprovarIntegerYString(String s) {
+
+        boolean resultado;
+
+        try {
+            Integer.parseInt(s);
+            Float.parseFloat(s);
+            resultado = true;
+        }catch(NumberFormatException e){
+            resultado = false;
+        }
+
+        return resultado;
+        
+    }
+
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        JSONArray json = response.optJSONArray("alumnos");
+
+        try {
+
+            for(int i = 0; i < json.length(); i++){
+                Alumno a = new Alumno();
+                JSONObject jsonObject = null;
+                jsonObject=json.getJSONObject(i);
+
+                a.setNombre(jsonObject.optString("nombre"));
+                a.setPrimer_apellido(jsonObject.optString("primer_apellido"));
+                a.setSegundo_apellido(jsonObject.optString("segundo_apellido"));
+                a.setDni(jsonObject.optString("dni"));
+                a.setFecha_nacimiento(jsonObject.optString("fecha_nacimiento"));
+                a.setGenero(jsonObject.optString("genero"));
+                a.setMano_dom(jsonObject.optString("mano_dom"));
+                a.setPie_dom(jsonObject.optString("pie_dom"));
+                a.setObservaciones(jsonObject.optString("observaciones"));
+                a.setMovil(jsonObject.optInt("movil"));
+                a.setPeso(jsonObject.optInt("peso"));
+                a.setAltura(jsonObject.optInt("altura"));
+
+                al_alumnos.add(a);
+
+
+            }
+
+    } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        }
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+
 }
