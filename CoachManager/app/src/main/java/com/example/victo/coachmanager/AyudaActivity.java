65faa2ebc1abd8648c7c921fff2a120de77cc54a
Óscar Gómez Fernández <oscaramelo_22@hotmail.com>
2018-06-03 17:14:47 +0200

    Vídeos incorporados en la app

--- CoachManager/app/src/main/java/com/example/victo/coachmanager/AyudaActivity.java
+++ CoachManager/app/src/main/java/com/example/victo/coachmanager/AyudaActivity.java
@@ -1,5 +1,7 @@
 package com.example.victo.coachmanager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,28 +28,34 @@ public class AyudaActivity extends AppCompatActivity implements View.OnClickList
 
         btnVolver = (ImageView) findViewById(R.id.btnVolverAyuda);
 
+        btnAlumnos.setOnClickListener(this);
+        btnGrupos.setOnClickListener(this);
+        btnEjercicios.setOnClickListener(this);
+        btnEntrenamientos.setOnClickListener(this);
+        btnVolver.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnAlumnosAyuda:{
-
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=WnOXeBgudn8")));
                 break;
             }
 
             case R.id.btnGruposAyuda:{
-
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=xv6ejdRIGuM")));
                 break;
             }
 
             case R.id.btnEjerciciosAyuda:{
-
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=fhqmLY2FDHY")));
                 break;
             }
 
             case R.id.btnEntrenamientoAyuda:{
-
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=UXD6ACNFjHg")));
                 break;
             }
 
